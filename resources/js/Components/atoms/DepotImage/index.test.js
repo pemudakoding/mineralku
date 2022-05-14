889f69a8d0824f0046875b9f5690b744feede37e
Stiven Katuuk <pemudakoding@gmail.com>
2022-05-14 17:29:19 +0800

    added depot image test

--- resources/js/Components/atoms/DepotImage/index.test.js
+++ resources/js/Components/atoms/DepotImage/index.test.js
@@ -1,10 +1,18 @@
 const renderer = require('react-test-renderer');
 import DepotImage from './index'
+import {Icon} from '@/Components';
 
 it('Depot Image showed verified badge', () => {
     const component = renderer.create(<DepotImage verified/>,);
+    const testInstance = component.root;
 
-    console.log(component.json());
+    expect(testInstance.findByType(Icon)).not.toBeFalsy();
+});
 
-    expect(true).toBeTruthy(true);
+
+it('Depot Image not showed verified badge', () => {
+    const component = renderer.create(<DepotImage/>,);
+    const testInstance = component.root;
+
+    expect(testInstance.findAllByType(Icon)).toHaveLength(0);
 });
