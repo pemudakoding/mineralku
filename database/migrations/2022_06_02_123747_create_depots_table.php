4261048a0b57e33362031b96040ef28af2aa8efe
Stiven Trizky Katuuk <pemudakoding@gmail.com>
2022-06-02 20:59:25 +0800

    added related table for order

--- database/migrations/2022_06_02_123747_create_depots_table.php
+++ database/migrations/2022_06_02_123747_create_depots_table.php
@@ -13,13 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('depots', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('whatsapp_numbers');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -31,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('depots');
     }
 };
---
+++ database/migrations/2022_06_02_125009_create_orders_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('depot_id')->nullable();
+            $table->foreignId('user_id')->nullable();
+            $table->integer('quantity');
+            $table->enum('shipping_detail', ['pickup', 'delivery']);
+            $table->smallInteger('is_delivery_now');
+            $table->timestamp('delivery_time');
+            $table->float('total_price');
+            $table->timestamps();
+
+            $table->foreign('depot_id')
+                ->references('id')
+                ->on('depots')
+                ->onDelete('set null');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
