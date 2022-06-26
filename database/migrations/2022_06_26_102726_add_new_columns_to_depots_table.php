71c685163dbeddb2504bf8730d3cd07b8ff974cb
Stiven Trizky Katuuk <pemudakoding@gmail.com>
2022-06-26 18:49:57 +0800

    added lat and long column for depots

---
+++ database/migrations/2022_06_26_102726_add_new_columns_to_depots_table.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('depots', function (Blueprint $table) {
+            $table->string('address')
+                ->after('name');
+
+            $table->foreignId('urban_village_id')
+                ->after('address')
+                ->nullable();
+
+            $table->foreignId('sub_district_id')
+                ->after('urban_village_id')
+                ->nullable();
+
+            $table->foreignId('district_id')
+                ->after('sub_district_id')
+                ->nullable();
+
+            $table->foreignId('province_id')
+                ->after('province_id')
+                ->nullable();
+
+            $table->foreign('urban_village_id')
+                ->references('id')
+                ->on('urban_villages')
+                ->onDelete('set null');
+
+            $table->foreign('sub_district_id')
+                ->references('id')
+                ->on('sub_districts')
+                ->onDelete('set null');
+
+            $table->foreign('district_id')
+                ->references('id')
+                ->on('districts')
+                ->onDelete('set null');
+
+            $table->foreign('province_id')
+                ->references('id')
+                ->on('provinces')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('depots', function (Blueprint $table) {
+            //
+        });
+    }
+};
