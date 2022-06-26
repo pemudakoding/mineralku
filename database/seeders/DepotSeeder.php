1b358503f5380f07c3a62d0b0559a94b95d49024
Stiven Trizky Katuuk <pemudakoding@gmail.com>
2022-06-26 19:06:48 +0800

    added afiqah depot seeder

--- database/seeders/DepotSeeder.php
+++ database/seeders/DepotSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Depot;
+use App\Models\District;
+use App\Models\Province;
+use App\Models\SubDistrict;
+use App\Models\UrbanVillage;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,8 +19,35 @@ class DepotSeeder extends Seeder
      */
     public function run()
     {
+        $urbanVillage = UrbanVillage::select('id')
+            ->where('name','Talise')
+            ->first()
+            ->id;
+
+        $subDistrict = SubDistrict::select('id')
+            ->where('name', 'Mantikulore')
+            ->first()
+            ->id;
+
+        $disctrict = District::select('id')
+            ->where('name', 'Kota Palu')
+            ->first()
+            ->id;
+
+        $province = Province::select('id')
+            ->where('name', 'Sulawesi Tengah')
+            ->first()
+            ->id;
+
         Depot::create([
-            'name' => 'Affifa Depot'
+            'name' => 'Depot Air Minum Isi Ulang Afiqah',
+            'address' => 'BTN Lagarutu CPI 2, Jl. Dayodara No.10, RW.008',
+            'urban_village_id' => $urbanVillage,
+            'sub_district_id' => $subDistrict,
+            'district_id' => $disctrict,
+            'province_id' => $province,
+            'lat' => '-0.8837565',
+            'long' => '119.894005'
         ]);
     }
 }
