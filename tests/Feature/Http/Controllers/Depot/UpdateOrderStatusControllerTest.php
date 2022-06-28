2a664bbc4532ca00d93ffea2a541a1d1ef8d2ed4
Stiven Trizky Katuuk <pemudakoding@gmail.com>
2022-06-28 21:18:22 +0800

    fixed test wrong endpoint

--- tests/Feature/Http/Controllers/Depot/UpdateOrderStatusControllerTest.php
+++ tests/Feature/Http/Controllers/Depot/UpdateOrderStatusControllerTest.php
@@ -15,35 +15,31 @@ class UpdateOrderStatusControllerTest extends TestCase
     {
         $order = Order::first();
 
-        $response = $this->patch('/depot/order/status',[
+        $response = $this->patch('/depot/orders/status',[
             'order_id' => $order->id,
             'status' => 'processing'
         ]);
 
-        $response1 = $this->patch('/depot/order/status',[
+        $response1 = $this->patch('/depot/orders/status',[
             'order_id' => $order->id,
             'status' => 'completed'
         ]);
 
-        $response2 = $this->patch('/depot/order/status',[
+        $response2 = $this->patch('/depot/orders/status',[
             'order_id' => $order->id,
             'status' => 'cancelled'
         ]);
 
-        $response->assertStatus(200);
-        $response1->assertStatus(200);
-        $response2->assertStatus(200);
-
-        $response->assertSeeText('1');
-        $response1->assertSeeText('1');
-        $response2->assertSeeText('1');
+        $response->assertStatus(302);
+        $response1->assertStatus(302);
+        $response2->assertStatus(302);
     }
 
     public function test_invalid_status_failing_when_update_order_status_controller()
     {
         $order = Order::first();
 
-        $response = $this->patch('/depot/order/status',[
+        $response = $this->patch('/depot/orders/status',[
             'order_id' => $order->id,
             'status' => 'invalid'
         ]);
@@ -53,7 +49,7 @@ class UpdateOrderStatusControllerTest extends TestCase
 
     public function test_invalid_order_id_failing_when_update_order_status_controller()
     {
-        $response = $this->patch('/depot/order/status',[
+        $response = $this->patch('/depot/orders/status',[
             'order_id' => 99999,
             'status' => 'invalid'
         ]);
