77db053b42c7d16cfed6f15d4929a8fa12d83841
Stiven Trizky Katuuk <pemudakoding@gmail.com>
2022-07-09 13:32:33 +0800

    modified redirect if unauthenticated

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,43 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+    /**
+     * Handle an unauthenticated user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $guards
+     * @return void
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    protected function unauthenticated($request, array $guards)
+    {
+        throw new AuthenticationException(
+            'Unauthenticated.', $guards, $this->redirectToIfUnauthenticated($request, $guards)
+        );
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    protected function redirectTo($request)
+    protected function redirectToIfUnauthenticated($request, $guards)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if(! $request->expectsJson()) {
+            foreach($guards as $guard) {
+                if($guard == 'depot'){
+                    return route('customers.index');
+                }
+
+                return route('login');
+            }
         }
     }
 }
