f1c2fdcafc02b2de6d11c6f59dadddd8cd359b1b
Stiven Trizky Katuuk <pemudakoding@gmail.com>
2022-07-09 14:06:20 +0800

    added new test case and modified test

--- tests/Feature/Http/Controllers/Depot/UpdateOrderStatusControllerTest.php
+++ tests/Feature/Http/Controllers/Depot/UpdateOrderStatusControllerTest.php
@@ -4,8 +4,6 @@ namespace Tests\Feature\Http\Controllers\Depot;
 
 use App\Models\Order;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UpdateOrderStatusControllerTest extends TestCase
@@ -39,20 +37,28 @@ class UpdateOrderStatusControllerTest extends TestCase
     {
         $order = Order::first();
 
-        $response = $this->patch('/depot/orders/status',[
-            'order_id' => $order->id,
-            'status' => 'invalid'
-        ]);
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user,'depot')
+            ->patch('/depot/orders/status',[
+                'order_id' => $order->id,
+                'status' => 'invalid'
+            ]);
 
         $response->assertInvalid();
     }
 
     public function test_invalid_order_id_failing_when_update_order_status_controller()
     {
-        $response = $this->patch('/depot/orders/status',[
-            'order_id' => 99999,
-            'status' => 'invalid'
-        ]);
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user,'depot')
+            ->patch('/depot/orders/status',[
+                'order_id' => 99999,
+                'status' => 'invalid'
+            ]);
 
         $response->assertInvalid();
     }
